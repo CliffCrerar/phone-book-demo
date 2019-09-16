196caa387240d84f558c2b6e88712e8d85b2a734
Cliff Crerar <cliff.crerar@gmail.com>
2019-09-16 22:45:07 +0200

    Fixed add new user bug, priduction build used the wrong API key for this CRUD operation

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,7 +4,7 @@ export const environment = {
   dataHostDev: 'http://localhost:3050',
   lookupKey: {
     getAllRecords: '1d963791-5450-4275-a614-7349b54df552',
-    postContact: '4308d16a-3065-44ed-9514-cb2c496b93b7',
+    postContact: '33183092-929b-44b4-9d7e-d2a8b3a98ead',
     deleteContact: '2033695f-12c5-477c-9be0-30319f3242ea',
     upsertContact: 'a6824247-ba78-4031-b04b-f2a7e980d3dd'
   }
